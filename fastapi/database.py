7401da9e03cf439c8e578220b29cf8b3b202ef09
Lycoriste <steventeng03@gmail.com>
2024-11-11 15:19:51 -0800

    Database credential changes

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE = 'postgresql://lycoris:rlgdaMwA9P7kPiz5jw8N@frontierdb.cb6cycsu2kgm.us-west-1.rds.amazonaws.com:5432/modeldb'
+load_dotenv()
+URL_DATABASE = os.getenv("DB_URL")
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
