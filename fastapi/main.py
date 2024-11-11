8d4992f4d5dc61386472bbb4cd197172984e492e
Lycoriste <steventeng03@gmail.com>
2024-11-11 00:26:20 -0800

    POST request for adding data to DB

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Annotated
+from typing import Annotated, Any
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Json
 from database import SessionLocal, engine
@@ -19,21 +19,29 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 ### Classes/tables stored and used in database ###
+#** Using dict instead of Json due to pydantic validation failing for ROBLOX
 
 class RINNBase(BaseModel):
     epoch: int
     strand: int
     training_score: int
-    training_output: Json
+    training_output: dict
 
-class RINNBase_data(BaseModel):
-    training_data: Json
+class RINNBase_data(BaseModel): 
+    training_data: dict
+
+### GET requests
+
+@app.get("/api/")
+async def root():
+    return "FASTAPI-BACKEND-ROOT"
 
 ### POST requests ###
 
-@app.post("/RINN_data/")
+@app.post("/api/RINN_data/")
 async def create_RINN_data(_RINN_data: RINNBase_data, db: db_dependency):
     db_RINN_data = models.RINN_data(_RINN_data.training_data)
+
     db.add(db_RINN_data)
     db.commit()
     db.refresh(db_RINN_data)
--- fastapi/models.py
+++ fastapi/models.py
@@ -12,9 +12,18 @@ class RINN(Base):
     training_score = Column(Integer)
     training_output = Column(JSON)
 
+    def __init(self, epoch, strand, training_score, training_output):
+        self.epoch = epoch
+        self.strand = strand
+        self.training_score = training_score
+        self.training_output = training_output
+
 ### Table for storing data retrieved from client
 class RINN_data(Base):
     __tablename__ = 'training_data'
 
     id = Column(Integer, primary_key=True, index=True)
     training_data = Column(JSON)
+
+    def __init__(self, training_data):
+        self.training_data = training_data
